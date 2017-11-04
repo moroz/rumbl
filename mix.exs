34d9e15c74b306a27e68a9bb1d58b86f6a641353
Karol M <dmuhafc@gmail.com>
2017-11-04 23:06:40 +0100

    Install Comeonin, add password hashing to User

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Rumbl.Mixfile do
   def application do
     [
       mod: {Rumbl, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :comeonin]
     ]
   end
 
@@ -40,7 +40,9 @@ defmodule Rumbl.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"}
     ]
   end
 
