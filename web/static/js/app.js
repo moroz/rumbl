533b194ffe6315175a4f85fad5bd91dc4eb45cc8
Karol Moroz <karol@mba>
2018-01-19 19:01:35 +0100

    Add videos#watch with JS and views

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
@@ -19,3 +19,12 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Player from "./player";
+let video = document.getElementById("video");
+
+if (video) {
+    Player.init(video.id, video.getAttribute("data-player-id"), () => {
+        console.log("player ready!");
+    });
+}
