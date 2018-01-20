ccb9c8f8908ffd606108bd6ac29b3bb1f2a5c444
Karol Moroz <karol@mba>
2018-01-20 16:45:23 +0100

    Stub adding annotations (without persistence)

--- web/views/watch/watch_view.ex
+++ web/views/watch/watch_view.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.WatchView do
   use Rumbl.Web, :view
 
   def player_id(video) do
-    ~r{^.*(?:youtu\.be/|\w+|v=)(?<id>[^#&?]*)}
+    ~r/^(?:https?:\/\/w{0,3}\.)?(?:youtu\.be\/|youtube\.com\/watch)\w*\?v=(?<id>[^#&?]*)/
     |> Regex.named_captures(video.url)
     |> get_in(["id"])
   end
