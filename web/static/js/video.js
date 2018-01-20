b4adb0877cc681777a93dd662fe89aaf91bd5782
Karol Moroz <karol@mba>
2018-01-20 18:01:45 +0100

    Send all persisted annotations upon join

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -1,53 +1,55 @@
 import Player from './player';
 
 let Video = {
-    init(socket, element) {
-        if(!element) return;
-        let playerId = element.getAttribute("data-player-id");
-        let videoId = element.getAttribute("data-id");
-        socket.connect();
-        Player.init(element.id, playerId, () => {
-            this.onReady(videoId, socket);
-        });
-    },
+  init(socket, element) {
+    if(!element) return;
+    let playerId = element.getAttribute("data-player-id");
+    let videoId = element.getAttribute("data-id");
+    socket.connect();
+    Player.init(element.id, playerId, () => {
+      this.onReady(videoId, socket);
+    });
+  },
 
-    onReady(videoId, socket) {
-        let msgContainer = document.getElementById("msg-container");
-        let msgInput = document.getElementById("msg-input");
-        let postButton = document.getElementById("msg-submit");
-        let vidChannel = socket.channel("videos:" + videoId);
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container");
+    let msgInput = document.getElementById("msg-input");
+    let postButton = document.getElementById("msg-submit");
+    let vidChannel = socket.channel("videos:" + videoId);
 
-        postButton.addEventListener("click", e => {
-            let payLoad = {body: msgInput.value, at: Player.getCurrentTime()};
-            vidChannel.push("new_annotation", payLoad)
-                .receive("error", e => console.log(e));
-            msgInput.value = "";
-        });
+    postButton.addEventListener("click", e => {
+      let payLoad = {body: msgInput.value, at: Player.getCurrentTime()};
+      vidChannel.push("new_annotation", payLoad)
+        .receive("error", e => console.log(e));
+      msgInput.value = "";
+    });
 
-        vidChannel.on("new_annotation", (resp) => {
-            this.renderAnnotation(msgContainer, resp);
-        });
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp);
+    });
 
-        vidChannel.join()
-            .receive("ok", resp => console.log("joined the video channel", resp))
-            .receive("error", reason => console.log("join failed", reason));
-    },
+    vidChannel.join()
+      .receive("ok", ({annotations}) => {
+        annotations.forEach( ann => this.renderAnnotation(msgContainer, ann));
+      })
+      .receive("error", reason => console.log("join failed", reason));
+  },
 
-    esc(str) {
-        let div = document.createElement("div");
-        div.appendChild(document.createTextNode(str));
-        return div.innerHTML;
-    },
+  esc(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  },
 
-    renderAnnotation(msgContainer, {user, body, at}) {
-        let template = document.createElement("div");
+  renderAnnotation(msgContainer, {user, body, at}) {
+    let template = document.createElement("div");
 
-        template.innerHTML = `<a href="#" data-seek="${this.esc(at)}">
+    template.innerHTML = `<a href="#" data-seek="${this.esc(at)}">
           <b>${this.esc(user.username)}</b>: ${this.esc(body)}
         </a>`;
-        msgContainer.appendChild(template);
-        msgContainer.scrollTop = msgContainer.scrollHeight;
-    }
+    msgContainer.appendChild(template);
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+  }
 };
 
 export default Video;
