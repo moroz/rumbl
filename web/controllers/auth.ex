53dc7c02f58a743347fc61982b3810c8b3683129
Karol Moroz <karol@mba>
2018-01-20 16:58:18 +0100

    Add user authentication for Sockets with tokens

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -42,9 +42,17 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  def put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def authenticate_user(conn, _opts) do
