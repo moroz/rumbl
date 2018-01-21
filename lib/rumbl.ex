dbdecbc4634e1dcf609031de2d9585bccd0a86a0
Karol Moroz <karol@mba>
2018-01-21 20:43:38 +0100

    Stub Rumbl.InfoSys with Supervisor

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -8,12 +8,9 @@ defmodule Rumbl do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Rumbl.Repo, []),
-      # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
-      # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
-      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      supervisor(Rumbl.Repo, []),
+      supervisor(Rumbl.InfoSys.Supervisor, [])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
